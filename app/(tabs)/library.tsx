1fa4d4d8bfc4b1c6ada83982a7c5e550982180d5
Amogelang Thomson <amogelangthompson@gmail.com>
2026-02-15 17:03:35 +0200

    library

--- app/(tabs)/library.tsx
+++ app/(tabs)/library.tsx
@@ -1,11 +1,30 @@
 import { View ,StyleSheet , ScrollView} from "react-native"; 
 import AppText from "../../components/AppText";
+import Search from "@/components/Search";
+import Play from "@/components/play";
+import { useMusicPlayer } from "@/context/PlayerComp";
+import { useRouter } from "expo-router";
 export default function Library() {
+    const {isPlaying , pause , resume , currentSong} = useMusicPlayer();
+    const router = useRouter();
     return(
         <View style = {styles.container}>
             <ScrollView style= {styles.container2}>
                 <AppText>View</AppText>
             </ScrollView>
+
+            <Search       
+                onPress={() => router.push("/(tabs)/settings")}
+            />
+            <Play 
+                onPress={() => isPlaying === true ? pause() : resume()} 
+                type = "saved"
+                isPlaying = {isPlaying}
+                artist= {currentSong?.artist}
+                tittle= {currentSong?.title}
+                image = {currentSong?.cover}
+                trackId= {currentSong?.id}
+            />
         </View>
     );
 }
