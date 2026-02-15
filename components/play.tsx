fde81b22a44f7aa00cd119fe85ad02169e400a1e
Amogelang Thomson <amogelangthompson@gmail.com>
2026-02-15 13:44:49 +0200

    play file

---
+++ components/play.tsx
@@ -0,0 +1,81 @@
+import { View , StyleSheet, TouchableOpacity, TextInput} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+
+type Props = {
+    type: "explore" | "saved" | "settings"; 
+    isPlaying: boolean;
+    onPress: () => void;
+};
+
+export default function Play({onPress , type , isPlaying} : Props){
+    
+
+    return(
+        <View style = {styles.iconContainer}>
+            {type === "explore" &&(
+                <TouchableOpacity style = {styles.iconSearch} onPress={onPress}>
+                
+                <Ionicons 
+                    name = {
+                    isPlaying === true 
+                    ? "pause"
+                    : "play"
+                }
+                size={20}
+                color="#ffffff"
+            />
+                
+            </TouchableOpacity>
+            )}
+            
+            </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    iconContainer : {
+        position : "absolute",
+        bottom : 50,
+        right : 20,
+        zIndex : 100,
+    },
+    iconSearch : {
+        width : 44, 
+        height : 44,
+        borderRadius : 22,
+        justifyContent : "center",
+        alignItems : "center",
+        backgroundColor : "#1e1e1e",
+    },
+    searchBar: {
+        flexDirection : "row",
+        alignItems : "center",
+        gap : 10,
+        paddingHorizontal : 15,
+        height : 40,
+        borderRadius : 100,
+        width : "70%",
+        marginLeft : "auto",
+        marginRight : "auto",
+        backgroundColor : "#1e1e1e",
+        
+        
+
+    },
+    input : {
+        color : "#ffffff",
+        fontSize : 16,
+        flex : 1,
+    },iconWrapper : {
+        position: "absolute",
+        top: 10, 
+        left: 0,
+    },iconBlur : {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+});
