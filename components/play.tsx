deead1da5982ea973fff450db72e30a31fed495a
Amogelang Thomson <amogelangthompson@gmail.com>
2026-02-15 17:04:52 +0200

    play

--- components/play.tsx
+++ components/play.tsx
@@ -1,21 +1,58 @@
-import { View , StyleSheet, TouchableOpacity, TextInput} from "react-native";
+import { LayoutAnimation , Platform , UIManager ,Animated ,View , StyleSheet, TouchableOpacity, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
+import AppText from "./AppText";
+import { ReactNode , useState , useRef, useEffect } from "react";
+import { useRouter } from "expo-router";
 
 type Props = {
     type: "explore" | "saved" | "settings"; 
     isPlaying: boolean;
     onPress: () => void;
+    artist?: string; 
+    tittle?: string ; 
+    image?: ReactNode;
+    trackId?: string
 };
 
-export default function Play({onPress , type , isPlaying} : Props){
+export default function Play({trackId , onPress , image , type , isPlaying , artist , tittle} : Props){
+    const [expanded, setExpanded] = useState(false);    
+    const router = useRouter();
     
+    if(Platform.OS === "android"){
+        UIManager.setLayoutAnimationEnabledExperimental?.(true);
+    }
+
+    const handlePress = () =>{
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+
+        if(!expanded) {
+            setExpanded(true); 
+            
+            return;
+        }
+
+        router.push({
+            pathname : "/(tabs)/musicPlayer",
+            params : {trackId}
+        });
+    }
+
+    useEffect(() => {
+        if(!expanded) return;
+        const timer = setTimeout (() =>{
+            LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+            setExpanded(false);
+        },6000);
+
+        return () => clearTimeout(timer);
+    },[expanded]);
+
 
     return(
         <View style = {styles.iconContainer}>
-            {type === "explore" &&(
+            {type === "explore"  &&(
                 <TouchableOpacity style = {styles.iconSearch} onPress={onPress}>
-                
+
                 <Ionicons 
                     name = {
                     isPlaying === true 
@@ -28,12 +65,55 @@ export default function Play({onPress , type , isPlaying} : Props){
                 
             </TouchableOpacity>
             )}
+
+
+            {type === "saved" &&(
+                
+                <TouchableOpacity  style = {[styles.iconsaved, expanded ? styles.expanded : styles.collapsed]}  onPress={handlePress}>
+                    
+                
+                <View style = {styles.main}>
+                 {expanded && (
+                    <View>
+                        <AppText style = {{fontSize: 18 , marginBottom: 2 , zIndex: 100}}>{tittle}</AppText>
+                        <AppText style = {{fontSize: 9 , zIndex: 100}}>{artist}</AppText>
+                    </View>
+                 )}
+                <TouchableOpacity onPress={expanded ? onPress : handlePress}>    
+                    <Ionicons 
+                        name = {
+                        isPlaying === true 
+                        ? "pause"
+                        : "play"
+                        }
+                     style = {{zIndex: 100}}
+                    size={20}
+                    color="#ffffff"
+                    />
+                </TouchableOpacity>      
+                
+              </View>
+                
+            </TouchableOpacity>
             
+            )}
             </View>
     );
 }
 
 const styles = StyleSheet.create({
+    main : {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        gap: 70 
+        
+    },image : {
+        width: 40 ,
+        height: 40, 
+        borderBottomStartRadius: 20,
+        borderTopStartRadius: 20
+    },
     iconContainer : {
         position : "absolute",
         bottom : 50,
@@ -47,6 +127,20 @@ const styles = StyleSheet.create({
         justifyContent : "center",
         alignItems : "center",
         backgroundColor : "#1e1e1e",
+    },
+    iconsaved : {
+        height : 44,
+        borderRadius : 22,
+        justifyContent : "center",
+        alignItems : "center",
+        overflow: "hidden",
+        backgroundColor : "#1e1e1e",
+    },collapsed : {
+        width: 44, 
+        alignItems: "center"
+    },expanded : {
+        width: 350 , 
+        paddingHorizontal: 12
     },
     searchBar: {
         flexDirection : "row",
