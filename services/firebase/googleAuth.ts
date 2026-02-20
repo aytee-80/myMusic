c2c34e1fb67d9a33d5d4b318d9179f6229999007
Amogelang <amogelangthompson@gmail.com>
2026-02-20 17:51:24 +0200

    Remove hardcoded Google client IDs
    Removed hardcoded Google client IDs from useGoogleAuth function.

--- services/firebase/googleAuth.ts
+++ services/firebase/googleAuth.ts
@@ -7,10 +7,10 @@ WebBrowser.maybeCompleteAuthSession();
 
 export function useGoogleAuth(){
     const [request, reponse, promptAsync] = Google.useAuthRequest({
-            clientId : "873109102732-njcmmu41oja15f1tb61annr6g4pmvsvm.apps.googleusercontent.com",
-            webClientId : "873109102732-njcmmu41oja15f1tb61annr6g4pmvsvm.apps.googleusercontent.com",
-            iosClientId : "873109102732-njcmmu41oja15f1tb61annr6g4pmvsvm.apps.googleusercontent.com", 
-            androidClientId : "873109102732-njcmmu41oja15f1tb61annr6g4pmvsvm.apps.googleusercontent.com"
+            clientId : "",
+            webClientId : "",
+            iosClientId : "", 
+            androidClientId : ""
     });
 
     const signinGoogle = async () => {
