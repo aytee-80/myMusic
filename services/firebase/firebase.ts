b1f2146e9cba4e5089f5d9fc85a42368ce3d3ff7
Amogelang Thomson <amogelangthompson@gmail.com>
2026-02-24 02:04:14 +0200

    firebase

--- services/firebase/firebase.ts
+++ services/firebase/firebase.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: process.env.EXPO_PUBLIC_API_KEY as string,
+    authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN as string,
+    projectId: process.env.EXPO_PUBLIC_PROJECT_ID as string,
+    storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET as string,
+    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID as string,
+    appId: process.env.EXPO_PUBLIC_APP_ID as string,
+    measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID as string
 };
+console.log("API:", process.env.EXPO_PUBLIC_API_KEY);
 
 const app = initializeApp(firebaseConfig); 
 export const fire = getAuth(app); 
